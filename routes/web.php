23b4caa2c5cfcd8a5dfcf9ebb37212c2ff79fc56
SamuelCelisL <samuelcelislizcano@gmail.com>
2025-08-23 12:19:34 -0500

    Add user management views and routes
    - Updated web routes to include user management paths for listing and creating users.
    - Created 'Usuario.index' view for displaying a list of users with search functionality and action buttons.
    - Added 'Usuario.action' view for user registration with a form to input user details.
    - Implemented a modal for user deletion confirmation in 'Usuario.index'.
    - Introduced a header and menu layout in 'plantilla.header' and 'plantilla.menu' for navigation.

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('plantilla.app');
 });
 Route::get('/app', function () {
     return view('plantilla.app');
+});
+Route::get('/usuario', function () {
+    return view('Usuario.index');
+});
+Route::get('/usuario/action', function () {
+    return view('Usuario.action');
 });
